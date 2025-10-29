14b291bb9184d83f13a1e63596c2a8598672a706
232187559+IadamI12@users.noreply.github.com <232187559+IadamI12@users.noreply.github.com>
2025-10-29 16:28:15 +0100

    Tesztek keszitese #1

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,11 +1,11 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class TorpedoStoreTest {
-
     @Test
     void fire_Success() {
         // Arrange
@@ -17,4 +17,24 @@ class TorpedoStoreTest {
         // Assert
         assertEquals(true, result);
     }
+    @Test
+    void fire_LowAmmunition(){
+        TorpedoStore store = new TorpedoStore(0);
+        assertThrows(IllegalArgumentException.class, () -> {
+            store.fire(2);
+        });
+    }
+    @Test
+    void ammo_IsEmpty(){
+        TorpedoStore store = new TorpedoStore(0);
+        assertEquals(true, store.isEmpty());
+    }
+    @Test
+    void ammo_Get(){
+        TorpedoStore store = new TorpedoStore(5);
+        assertEquals(5, store.getTorpedoCount());
+    }
+
+
+
 }
